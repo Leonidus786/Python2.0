59b3239c151a0f78fba4d143faf7540fa3381552
Leonidus786 <abhi.upadh@gmail.com>
2024-04-15 23:34:34 +0530

    input--output

---
+++ Day_1/Output/Input/README.md
@@ -0,0 +1,10 @@
+# print() --> Output function --> Jo screen/terminal/console me output deta h mtlb result dikhata hai
+# If you need to print something as output then always use print() method/function
+
+# Python Latest version --> https://www.python.org/ 
+
+# Python -- cool language
+# Indentation -- Extra White spaces -- 4 spaces = 1 indent----- tab
+
+
+
---
+++ Day_1/Output/Input/main.py
@@ -0,0 +1,26 @@
+print("Hello World!")
+
+# This is single line comment. --> Ctrl + /
+# print() is used to print something
+'''Multi
+Line
+comment'''
+
+"""This 
+is 
+also 
+a
+comment"""
+
+# comments are ignored by interpreter.
+
+
+# print("Krusabh")
+# print("Akshat")
+
+# print("What is your name? ")
+
+# input() -- Take input from user/hum --- 
+
+
+input("What is your name? ")
